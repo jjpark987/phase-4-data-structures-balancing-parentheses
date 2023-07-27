cd582c3be137f6fff0e8a5f845e039b85b201a00
Jason Park <jjpark987@gmail.com>
2023-07-27 12:39:05 -0700

    commit tree cf62f1453aa9ae733525ad34f3ca05bb888518f7

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,21 @@
 require_relative './stack'
 
-# your code here
+def balancing_parentheses string
+    balance = 0
+    stack = Stack.new
+    string.chars.each do |char|
+        if char == '('
+            stack.push('(')
+            balance += 1
+        else
+            if stack.peek == '(' 
+                stack.pop
+                balance -= 1
+            else
+                stack.push(')')
+                balance += 1
+            end
+        end
+    end
+    balance
+end
